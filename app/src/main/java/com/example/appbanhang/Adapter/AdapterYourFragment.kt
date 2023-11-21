c056f06bb00760bf4eef2323aafe524fc10ec934
HienNguyen130301 <nmhien1303@gmail.com>
2023-11-21 12:10:49 +0700

    Update UI

---
+++ app/src/main/java/com/example/appbanhang/Adapter/AdapterYourFragment.kt
@@ -0,0 +1,6 @@
+package com.example.appbanhang.Adapter
+
+import androidx.recyclerview.widget.RecyclerView
+
+class AdapterYourFragment(val itemlist: ArrayList<>): RecyclerView.Adapter<AdapterRecommended.viewHolder>() {
+}
--- app/src/main/java/com/example/appbanhang/FragmentParent/YouFragment.kt
+++ app/src/main/java/com/example/appbanhang/FragmentParent/YouFragment.kt
@@ -9,16 +9,17 @@ import com.example.appbanhang.R
 
 class YouFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_you, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?){
+        super.onViewCreated(view, savedInstanceState)
+
     }
 
 }
